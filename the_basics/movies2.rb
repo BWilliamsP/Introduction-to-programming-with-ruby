ab9cde748a70266956c49ea6c985f6542fc033c6
Ben Williams <Benjaminwilliamssda@gmail.com>
2022-11-18 09:36:20 -1000

    Re doing practices to heelp solidify

---
+++ the_basics/movies2.rb
@@ -0,0 +1,12 @@
+movies = {
+  :hello => '1975',
+   :bye => '2004',
+    :yes => '2013',
+     :no => '2001',
+      :yo => '1981'}
+      
+puts movies [:hello]
+puts movies [:bye]
+puts movies [:yes]
+puts movies [:no]
+puts movies [:yo]
